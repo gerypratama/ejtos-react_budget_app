a7ba53b5d5796f191fbc440fd797fbbcd26c7e1f
Gery <gerypratama.o@gmail.com>
2023-11-11 22:18:36 +0700

    Project practice commit

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,1 +1,110 @@
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
+const AllocationForm = (props) => {
+  const { dispatch, remaining } = useContext(AppContext);
+
+  const [name, setName] = useState("");
+  const [cost, setCost] = useState("");
+  const [action, setAction] = useState("");
+
+  const submitEvent = () => {
+    if (cost > remaining) {
+      alert("The value cannot exceed remaining funds  £" + remaining);
+      setCost("");
+      return;
+    }
+
+    const expense = {
+      name: name,
+      cost: parseInt(cost),
+    };
+
+    if (action === "Reduce") {
+      dispatch({
+        type: "RED_EXPENSE",
+        payload: expense,
+      });
+    } else {
+      dispatch({
+        type: "ADD_EXPENSE",
+        payload: expense,
+      });
+    }
+  };
+  return (
+    <div>
+      <div className="row">
+        <div className="input-group mb-3" style={{ marginLeft: "2rem" }}>
+          <div className="input-group-prepend">
+            <label htmlFor="inputGroupSelect01" className="input-group-text">
+              Department
+            </label>
+          </div>
+          <select
+            id="inputGroupSelect01"
+            className="custom-select"
+            onChange={(e) => setName(e.target.value)}
+          >
+            <option defaultValue>Choose...</option>
+            <option value="Marketing" name="marketing">
+              Marketing
+            </option>
+            <option value="Sales" name="sales">
+              Sales
+            </option>
+            <option value="Finance" name="finance">
+              Finance
+            </option>
+            <option value="HR" name="hr">
+              HR
+            </option>
+            <option value="IT" name="IT">
+              IT
+            </option>
+            <option value="Admin" name="Admin">
+              Admin
+            </option>
+          </select>
+
+          <div className="input-group-prepend" style={{ marginLeft: "2rem" }}>
+            <label htmlFor="inputGroupSelect02" className="input-group-text">
+              Allocation
+            </label>
+          </div>
+          <select
+            className="custom-select"
+            id="inputGroupSelect02"
+            onChange={(event) => setAction(event.target.value)}
+          >
+            <option defaultValue value="Add" name="Add">
+              Add
+            </option>
+            <option value="Reduce" name="Reduce">
+              Reduce
+            </option>
+          </select>
+
+          <input
+            type="number"
+            id="cost"
+            value={cost}
+            style={{ marginLeft: "2rem", fontSize: 10 }}
+            onChange={(e) => setCost(e.target.value)}
+            required
+          />
+
+          <button
+            className="btn btn-primary"
+            onClick={submitEvent}
+            style={{ marginLeft: "2rem" }}
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllocationForm;
